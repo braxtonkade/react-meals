3d3779ff5b572f2e624750939821f9b4e2847832
Braxton Dalton <braxtonkade1024@gmail.com>
2023-08-28 09:57:17 -0400

    Build LoginButton

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import styles from "../css-modules/Header.module.css";
 import meals from "../assets/images/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
+import LoginButton from "./LoginButton";
 
 const Header = () => {
   return (
     <>
       <div className={styles.header}>
         <h2>ReactMeals</h2>
-        <HeaderCartButton />
+        <div className={styles["btn-container"]}>
+          <HeaderCartButton />
+          <LoginButton />
+        </div>
       </div>
       <div className={styles["main-image"]}>
         <img src={meals} alt="Table with assortment of food" />
---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "../css-modules/HeaderCartButton.module.css";
+
+const LoginButton = () => {
+  return (
+    <>
+      <button className={styles.button}>Login</button>
+    </>
+  );
+};
+
+export default LoginButton;
